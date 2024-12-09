843cb72ef65fbe599e5f9b73ccaf48833ba998aa
daksh saini <157453970+iamdakshsainii@users.noreply.github.com>
2024-12-09 11:47:25 +0000

    conversions

--- Basics/conversions.js
+++ Basics/conversions.js
@@ -1,4 +1,19 @@
+// conversion of num to String
 let number= 34;
 let result= String(number);
 console.log(result);
 console.log(typeof result);
+
+// if unusual then output will be 
+// NaN
+let num="32abc";
+let num1=Number(num);
+console.log(typeof num1);
+console.log(num1);
+
+// boolean converison
+// if not 0 => true
+// if 0 then false
+let num3= 12;
+let num4= Boolean(num3);
+console.log(num4);
