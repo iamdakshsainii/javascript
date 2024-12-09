50b5e800f04dc8acdc5d5eb13477e7b6ddc8c615
daksh saini <157453970+iamdakshsainii@users.noreply.github.com>
2024-12-09 11:41:41 +0000

    variable define

--- Basics/variables.js
+++ Basics/variables.js
@@ -1,1 +1,27 @@
-console.log("Daksh Saini");
+// prefer camelCase
+
+// Variables declared with let have Block Scope
+//Variables declared with let must be Declared before use
+//Variables declared with let cannot be Redeclared in the same scope
+
+// so basic to learn here is 
+// const -> scope hota h, not update and not redeclare
+// let -> scope h , update kr skte hai but not redeclare
+// var -> Variables declared with var can be redeclared without error.
+
+// Let
+let fname = "Daksh";
+// update possible not redeclared
+fname="Saini"; 
+
+// Var
+var fullName= "Daksh Saini";
+fullName= "Ashish Soni";
+var fullName=12;
+
+//const
+const fullname2 = "Saurabh yadav";
+// fullname2= "aditya agarwal"; // give error
+
+// print
+console.table([ fname, fullName, fullname2]);
