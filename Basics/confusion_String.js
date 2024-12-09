e5dfa3308e25c5729b57611c2ad365a95fe299c1
daksh saini <157453970+iamdakshsainii@users.noreply.github.com>
2024-12-09 12:08:24 +0000

    confusion cleared

---
+++ Basics/confusion_String.js
@@ -0,0 +1,16 @@
+// String + something = String concatenation
+// Number + Number = Addition
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1+2+"2"); // first js check 1+2 => 3 and then string so convert and give 32
+// make it better and use 
+console.log((1+2)+"2"); 
+
+// increment and decrement
+// If used postfix, with operator after operand 
+// (for example, x++), the increment operator increments
+// and returns the value before incrementing.
+
+// If used prefix, with operator before operand
+// (for example, ++x), the increment operator increments 
+// and returns the value after incrementing.
